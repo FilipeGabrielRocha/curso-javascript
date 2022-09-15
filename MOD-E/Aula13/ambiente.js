538bf9004eb51aa92a7d02acdf25b84fd75bb516
Filipe Gabriel Rocha <filiperochaprogramador@gmail.com>
2022-09-15 11:03:48 -0300

    Aula 13 JavaScript

---
+++ MOD-E/Aula13/ambiente.js
@@ -0,0 +1,13 @@
+// REPETIÇÃO COM TESTE LÓGICO NO FINAL
+var cont = 1
+do {
+    console.log(`${cont}° Tudo bem?`)
+    cont += 1
+} while (cont <= 6)
+
+// REPETIÇÃO COM TESTE LÓGICO NO INÍCIO
+var cont = 1
+while (cont <= 6) {
+    console.log(`${cont}° Tudo bem?`)
+    cont += 1
+}
