20dde5195be607c0a0fba01b76052b6eea901157
Filipe Gabriel Rocha <filiperochaprogramador@gmail.com>
2022-09-15 09:42:12 -0300

    Exercicio parte 2

--- MOD-D/Exercicio-parte1/js/script.js
+++ MOD-D/Exercicio-parte1/js/script.js
@@ -1,17 +1,17 @@
-var corpo = window.document.body
-var horaAtual = new Date()
-var hora = horaAtual.getHours()
-var min = horaAtual.getMinutes()
-var res = window.document.getElementById('res')
-var imagem = window.document.getElementById('imagem')
-res.innerHTML = `<p>Agora são ${hora}:${min} horas.</p>`
-if (hora < 12 ) {
-    corpo.style.background = 'yellow'
-    imagem.innerHTML += `<img src="image/manha.jpg" alt="">`
-} else if (hora <= 18) {
-    corpo.style.background = 'orange'
-    imagem.innerHTML += `<img src="image/tarde.jpg" alt="">`
-} else {
-    corpo.style.background = 'darkblue'
-    imagem.innerHTML += `<img src="image/noite.jpg" alt="">`
+function carregar() {
+    var res = window.document.getElementById('res')
+    var img = window.document.getElementById('imagem')
+    var horaAtual = new Date()
+    var hora = horaAtual.getHours()
+    res.innerHTML = `<p>Agora são ${hora} horas.</p>`
+    if (hora < 12 ) {
+        window.document.body.style.background = '#e2cd9f'
+        img.src = 'image/manha.jpg'
+    } else if (hora < 18) {
+        window.document.body.style.background = '#b9846f'
+        img.src = 'image/tarde.jpg'
+    } else {
+        window.document.body.style.background = '#515154'
+        img.src = 'image/noite.jpg'
+    }
 }
